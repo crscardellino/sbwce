f492f6117ae4132edd0f4992e5f3f5a696ed4a85
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-16 20:16:16 +0000

    Adding tarfile member sanitization to extractall()

--- get_infoleg.py
+++ get_infoleg.py
@@ -26,7 +26,26 @@ with open(INFOLEG_TMP, 'wb') as fh:
 
 with tarfile.open(INFOLEG_TMP, 'r') as fi, open(INFOLEG_FIL, 'w') as fo:
     print("Extracting file...", file=sys.stderr)
-    fi.extractall(path="/tmp")
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(fi, path="/tmp")
 
     print("Parsing files...", file=sys.stderr)
     for fname in tqdm(os.listdir(INFOLEG_OUT)):
